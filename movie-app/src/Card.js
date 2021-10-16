271fc4163a9ba843ee46396aae78059837a5571b
DivyanshSareen <sareendivyansh@gmail.com>
2021-10-16 11:52:56 +0530

    adjusted card.js for tmdb data

--- movie-app/src/Card.js
+++ movie-app/src/Card.js
@@ -2,16 +2,16 @@
 import './Card.css'
 const Card = (props) => {
     return ( 
-    <div key={props.name} className="card">
-            <img src={props.poster} alt={props.name+"_image"}  className="card-img"/>
+    <div className="card">
+            <img src={"https://image.tmdb.org/t/p/w500"+props.poster} alt={props.name+"_image"}  className="card-img"/>
         <div className="card-content">
             <h3 className="card-title">
                 {props.name}
             </h3>
             <div className="card-det">
-                <p className="card-date">2021</p>
+                <p className="card-date">{props.date}</p>
                 <span>|</span>
-                <p className="card-dur">144m</p>
+                <p className="card-dur">{props.rate}</p>
             </div>
         </div>
     </div>);
