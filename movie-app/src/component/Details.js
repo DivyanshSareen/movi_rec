dfb722c585f4694d1e23cf3bcb2c26e6632421fb
DivyanshSareen <sareendivyansh@gmail.com>
2021-10-28 19:39:27 +0530

    added jsx for details page

--- movie-app/src/component/Details.js
+++ movie-app/src/component/Details.js
@@ -18,7 +18,23 @@ const Details = (props) => {
         <div>
         {isBusy ? (<h1>loading</h1>) :
             (<div>
-                <div><img src={"https://image.tmdb.org/t/p/w500"+movie.poster_path} alt={props.name+"_image"}/></div>
+                <div>
+                    <img src={"https://image.tmdb.org/t/p/w500"+movie.poster_path} alt={props.name+"_image"}/>
+                </div>
+                <div>
+                    <h2>{movie.title}</h2>
+                    <div>
+                        <span>{movie.vote_average}</span>
+                        <span>{movie.runtime}</span>
+                    </div>
+                    <p>{movie.overview}</p>
+                    <div>
+                        <span>Released: </span> <span>{movie.release_date}</span>
+                        <span>Genre: </span> <span>{movie.genres.map(e => e.name)}</span>
+                        <span>Language: </span> <span>{movie.spoken_languages.map(e => e.english_name)}</span>
+                        <span>Production: </span> <span>{movie.production_companies.map(e => e.name)}</span>
+                    </div>
+                </div>
             </div>)
         }
         </div>
