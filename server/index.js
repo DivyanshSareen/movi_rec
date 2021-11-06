8a2da594d680139ae406786d98701b62dfeb9c7c
DivyanshSareen <sareendivyansh@gmail.com>
2021-11-06 23:36:10 +0530

    api is now capable of providing recommendations
    server now gives recommendations on /rec/:id path

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 import dotenv from 'dotenv';
 import fetch from 'node-fetch';
-import express from 'express';
+import express, { json } from 'express';
 import cors from 'cors';
+import { readFileSync } from 'fs';
+
+
+
 
 dotenv.config();
 const app = express();
@@ -25,14 +29,41 @@ async function getdata(req, res, next){
   next();
 }
 
-  async function requestMovies(req, res, next){
+async function requestMovies(req, res, next){
+  const r = await fetch("https://api.themoviedb.org/3/movie/"+req.params.type+"?api_key="+process.env.API_KEY+"&page=1");
+  const json = await r.json();
+  req.movies = json;
+  console.log(req.movies);
+  next();
+}
 
-    const r = await fetch("https://api.themoviedb.org/3/movie/"+req.params.type+"?api_key="+process.env.API_KEY+"&page=1");
-    const json = await r.json();
-    req.movies = json;
-    console.log(req.movies);
-    next();
+async function getRecommendations(req, res, next){
+  let id = req.params.id;
+  const raw_data = readFileSync('./movie_rec.json');
+  const data = JSON.parse(raw_data);
+  // req.dat = data["1891"];
+  if(id in data)
+  {
+    req.dat = data[id]
   }
+  else
+  {
+    let temp = [];
+    const r = await fetch("https://api.themoviedb.org/3/movie/"+id+"/recommendations?api_key="+process.env.API_KEY);
+    const data = await r.json();
+    for(let x of data["results"])
+    {
+      temp.push(x.id)
+    }
+    req.dat = {"rec_ids": temp};
+  }
+  next();
+}
+app.use("/rec/:id", getRecommendations);
+app.get("/rec/:id", (req, res) =>{
+   res.send(req.dat);
+});
+
 app.use("/movie/:type",requestMovies);
 app.get("/movie/:type", (req, res) =>{
     // console.log(req.movies)
